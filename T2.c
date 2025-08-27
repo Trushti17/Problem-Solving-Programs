644150432e2732f769bf97ecd70100342c7b6fa6
mahi <mahi@gmail.com>
2025-08-27 17:12:41 +0530

    sec. program done...!

---
+++ T2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Name : Trushti Vekariya \n");
+    printf("Adress : Gandhinagar\n");
+    printf("Birth-date: 17 July, 2005\n");
+
+    return 0;
+
+}
+

cb7ef66ca6a8788dd34fccbaf458a393560c227c
mahi <mahi@gmail.com>
2025-09-03 20:28:09 +0530

    done

--- T29.java
+++ T29.java
@@ -9,10 +9,25 @@ public class T29
         System.out.print("Enter four digit number...");
         String num = Scanner.next();
 
-        String str = String.valueOf(num);
-        System.out.println(str); 
-
-        
+        if (num.matches("\\d{4}"))
+        {
+            String[] digitWords = 
+            {
+                "Zero", "One", "Two", "Three", "Four",
+                "Five", "Six", "Seven", "Eight", "Nine"
+            };
+            System.out.print("Output: ");
+            for (int i = 0; i < num.length(); i++) 
+            {
+                char ch = num.charAt(i);
+                int digit = Character.getNumericValue(ch);
+                System.out.print(digitWords[digit] + " ");
+            }
+        }
+         else
+        {
+            System.out.println("Invalid input. Please enter a 4-digit number.");
+        }
 
         Scanner.close();
     }    
